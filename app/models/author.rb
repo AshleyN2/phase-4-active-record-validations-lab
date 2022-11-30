7602467f3a7c2e8afa099db442afcc7083fa3947
Ashley <ashleynjeri7@gmail.com>
2022-11-30 13:11:07 +0300

    Passed Lab

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
-class Author < ApplicationRecord
+class Author < ApplicationRecord 
+   # is expected to validate that :name cannot be empty/falsy
+   # is expected to validate that :name is case-sensitively unique
     validates :name, presence: true,  uniqueness: true
-    validates :phone_number, length: { is: 10 }
+    validates :phone_number, length: { is: 10 } # is expected to validate that the length of :phone_number is 10
 end
