e087e88620e887570e2063554c818d7d46111d1e
Ashley <ashleynjeri7@gmail.com>
2022-11-30 13:08:09 +0300

    Passed Tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence:true # is expected to validate that :title cannot be empty/falsy
+    validates :content, length: { minimum: 250} #  is expected to validate that the length of :content is at least 250
+    validates :summary, length: { maximum: 250} #  is expected to validate that the length of :summary is at most 250
+    validates :category, inclusion: { in: %w( Fiction Non-Fiction ) } # is expected to validate that :category is either ‹"Fiction"› or ‹"Non-Fiction"›
+    validate :clickbait?
+
+   # Custom validations: title is clickbait
+   # is expected to allow :title to be ‹"You Won't Believe These True Facts"›
+   # is expected not to allow :title to be ‹"True Facts"
+    def clickbait?
+        if title && !title.match(/Won't Believe|Secret|Top [0-9]|Guess/)
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
